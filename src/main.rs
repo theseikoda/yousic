43c24b70d45a946bdea1e4ae8cf5ef88922e4eb1
sir naji <47813733+theseikoda@users.noreply.github.com>
2023-05-11 18:00:32 +0200

    Move discord presence to dedicated file

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::sync::Mutex;
-
-use discord_rich_presence::{
-    activity::{self, Assets},
-    DiscordIpc, DiscordIpcClient,
-};
+use discord_presence::DiscordPresence;
 use song::Song;
 use tauri::{Manager, State};
 
+mod discord_presence;
 mod song;
-
-pub struct DiscordPresence {
-    pub client: Mutex<DiscordIpcClient>,
-}
-
-impl DiscordPresence {
-    pub fn new(application_id: u64) -> Self {
-        let client = Mutex::new(DiscordIpcClient::new(&application_id.to_string()).unwrap());
-        Self { client }
-    }
-
-    pub fn start(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        _ = self.client.lock().unwrap().connect();
-        Ok(())
-    }
-
-    pub fn update_presence(&self, song: Song) -> Result<(), Box<dyn std::error::Error>> {
-        let details = song.title.clone();
-        let state = format!("By {}", song.author);
-
-        if let Some(image) = &song.image {
-            self.client.lock().unwrap().set_activity(
-                activity::Activity::new()
-                    .state(&state)
-                    .details(&details)
-                    .assets(Assets::new().large_image(image).large_text(&details)),
-            )?;
-        } else {
-            self.client.lock().unwrap().set_activity(
-                activity::Activity::new()
-                    .state(&state)
-                    .assets(Assets::new().small_text(&details))
-                    .details(&details),
-            )?;
-        };
-
-        Ok(())
-    }
-}
 
 #[tauri::command]
 fn on_new_song_playing(song: serde_json::Value, discord_presence: State<'_, DiscordPresence>) {
